065f4f86953c18e2c69ef15bbfdba81cda42b7b5
g <g>
2024-03-01 11:35:46 -0300

    js

---
+++ Aula 03-01/exemplos_aula/js/scripts1.js
@@ -0,0 +1,15 @@
+document.getElementById('mensagem').innerHTML = 'Hello World!';
+
+console.log('Hello World!');
+
+//window.alert("Hello World");
+
+//alert("Hello World");
+
+console.log(typeof 5);
+
+console.log(typeof 5.2);
+
+console.log(typeof 'String');
+
+console.log(typeof true);
