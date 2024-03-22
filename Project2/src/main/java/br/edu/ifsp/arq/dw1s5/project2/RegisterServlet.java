cf9967dc59ff43594aaf6a8fe98f75c880823a5f
Your Name <you@example.com>
2024-03-22 08:38:27 -0300

    a

--- Project2/src/main/java/br/edu/ifsp/arq/dw1s5/project2/RegisterServlet.java
+++ Project2/src/main/java/br/edu/ifsp/arq/dw1s5/project2/RegisterServlet.java
@@ -2,7 +2,7 @@ package br.edu.ifsp.arq.dw1s5.project2;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-//import java.util.Iterator;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/register")
-public class RegisterServlet extends HttpServlet{
+public class RegisterServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
 
@@ -20,32 +20,32 @@ public class RegisterServlet extends HttpServlet{
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		//configurar a codificação de caracteres da requisição
+		// configurar a codificação de caracteres da requisição
 		req.setCharacterEncoding("UTF-8");
-		//obter os dados da requisição
-		String fullname = req.getParameter("fullname");
+		// obter os dados da requisição
+		String fullName = req.getParameter("fullName");
 		String email = req.getParameter("email");
 		String options[] = req.getParameterValues("options");
 		String selectedOptions = "";
-		for (String option : options){
+		for(String option : options) {
 			selectedOptions += option + " ";
 		}
-		//escrever a re resposta
-		resp.setCharacterEncoding("text/html;charset=UTF-8");
+		// escrever a resposta
+		resp.setContentType("text/html;charset=UTF-8");
 		PrintWriter writer = resp.getWriter();
 		writer.println("<!DOCTYPE html>");
 		writer.println("<html lang=\"pt-BR\">");
 		writer.println("<head>");
 		writer.println("\t<meta charset=\"UTF-8\">");
-		writer.println("\t<title>Página de resposta - News List</title>");
+		writer.println("\t<title>Página de Resposta - News List</title>");
 		writer.println("</head>");
 		writer.println("<body>");
-		writer.println("\t<h1>Cadastro Realizado com sucesso!</h1>");
-		writer.println("\t<h2>Nome completo:" + fullname + "</h2>");
-		writer.println("\t<h2>E-mail:" + email + "</h2>");
-		writer.println("\t<h2>Interesses:" + selectedOptions + "</h2>");
+		writer.println("\t<h1>Cadastro realizado com sucesso!</h1>");
+		writer.println("\t<h2>Nome completo: "+ fullName +"</h2>");
+		writer.println("\t<h2>E-mail: "+ email +"</h2>");
+		writer.println("\t<h2>Interesses: "+ selectedOptions +"</h2>");
 		writer.println("</body>");
-		writer.println("</html>");
+		writer.println("</html");
 		writer.close();
 	}
 
@@ -53,4 +53,7 @@ public class RegisterServlet extends HttpServlet{
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		doGet(req, resp);
 	}
+	
+	
+
 }
